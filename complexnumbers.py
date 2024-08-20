c9a769c6785bfa6402247f848b030e7ff6e35ef9
Ruan Batista Rodrigues <145821191+zzruanxx@users.noreply.github.com>
2024-08-20 00:43:05 -0300

    Add files via upload
    treino python segunda feira

---
+++ complexnumbers.py
@@ -0,0 +1,10 @@
+import math
+
+z = 3 + 4j
+
+modulo_z = abs(z)
+
+parte_real = z.real
+parte_imaginaria = z.imag
+conjugado_z = z.conjugate()
+print(modulo_z, parte_real, parte_imaginaria, conjugado_z)
